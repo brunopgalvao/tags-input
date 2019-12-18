1ffeaae66693066e57339ae02785449ed22cc896
Bruno Galvao <brunopgalvao@gmail.com>
2019-12-18 09:47:36 -0500

    tags

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import TagsInput from './components/TagsInput'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <TagsInput/>
+      </div>
   );
-}
+};
 
 export default App;
